74a9efba7d63687e81d79bc71219bcf9b9fae1fd
Arnaud Morin <arnaud.morin@gmail.com>
2017-01-03 21:57:43 +0100

    Remove LC_BASTION

--- files/tmux/tmux.sh
+++ files/tmux/tmux.sh
@@ -5,17 +5,12 @@ SESSION="$1"
 # Set title of terminator
 echo -ne "\033]0;$SESSION\007"
 
-# Check if user config file exits
-if [ -e /etc/tmux/tmux-$LC_BASTION.conf ] ; then
-    CONFIG=/etc/tmux/tmux-$LC_BASTION.conf
-else
-    CONFIG=/etc/tmux/tmux.conf
-fi
+CONFIG=/etc/tmux/tmux.conf
 
 # Check if SESSION exits, if not, create it without connecting to it
-check=$(tmux -L $LC_BASTION ls -F "#{session_name}" | egrep "^$SESSION\$")
+check=$(tmux -L root ls -F "#{session_name}" | egrep "^$SESSION\$")
 if [ "Z${check}" = "Z" ] ; then
-    tmux -L $LC_BASTION -f $CONFIG new -s $SESSION -d
+    tmux -L root -f $CONFIG new -s $SESSION -d
 fi
 
-tmux -L $LC_BASTION -f $CONFIG attach -t $SESSION
+tmux -L root -f $CONFIG attach -t $SESSION
