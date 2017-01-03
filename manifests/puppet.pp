a69cbc75d3f9dfc5e0ece425bdf7ac3f6792cda1
Arnaud Morin <arnaud.morin@gmail.com>
2017-01-03 19:58:07 +0100

    Add puppet

---
+++ manifests/puppet.pp
@@ -0,0 +1,23 @@
+#
+# Mailops Team
+#
+# Class to configure puppet agent
+
+class base::puppet {
+  service { 'puppet':
+    ensure      =>  running,
+  }
+
+  # Set some aliases
+  bash::alias { 'p':
+    content => 'puppet agent --test',
+  }
+
+  bash::alias { 'pe':
+    content => 'puppet agent --enable',
+  }
+
+  bash::alias { 'pd':
+    content => 'puppet agent --disable',
+  }
+}
