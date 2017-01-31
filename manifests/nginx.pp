11459e1acf88c197d4cb84d40bec68f982fde7f2
jlejeune <mail.julien.le.jeune@gmail.com>
2017-01-31 17:27:49 +0100

    Add config for default nginx vhost

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -11,18 +11,50 @@ class base::nginx {
   }
 
   # Main files
-  file { '/var/www/':
-    ensure  => directory,
-    owner   => 'www-data',
-    group   => 'www-data',
+  file { '/var/www/html/':
+    ensure    => directory,
+    owner     => 'www-data',
+    group     => 'www-data',
+    recurse   => true,
   }
 
+  file { "/var/www/html/.well-known":
+    ensure    => directory,
+    owner     => 'www-data',
+    group     => 'www-data',
+    require   => File['/var/www/html/'],
+  }
+
+  file { '/var/www/html/index.html':
+    ensure    => file,
+    content   => template("${module_name}/nginx-default-index"),
+    owner     => 'www-data',
+    group     => 'www-data',
+    require   => File['/var/www/html/'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure    => file,
+    content   => template("${module_name}/nginx-default-vhost"),
+    require   => Package['nginx'],
+    notify    => Service['nginx'],
+  }
+  ->
   file { '/etc/nginx/sites-enabled/default':
-    ensure  => absent,
-    notify  => Service['nginx'],
+    ensure    => link,
+    target    => '/etc/nginx/sites-available/default',
+    notify    => Service['nginx'],
+  }
+
+  letsencrypt::certonly { 'mailops':
+    domains              => ['mailops.fr'],
+    plugin               => 'webroot',
+    webroot_paths        => ['/var/www/html'],
+    manage_cron          => true,
+    cron_success_command => '/bin/systemctl reload nginx.service',
   }
 
   service { 'nginx':
-    ensure  => running,
+    ensure    => running,
   }
 }
